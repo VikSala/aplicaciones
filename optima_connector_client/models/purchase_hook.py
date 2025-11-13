2d4d8a30f07f91c6ae4197b22c4d4f36c7435e6a
VikSala <vsz11697@gmail.com>
2025-11-13 17:29:59 +0100

    Puerto B2B

--- optima_connector_client/models/purchase_hook.py
+++ optima_connector_client/models/purchase_hook.py
@@ -8,7 +8,7 @@ class PurchaseOrder(models.Model):
         """Envía el pedido de compra al Odoo del proveedor (Óptima)."""
         res = super().button_confirm()
 
-        OPTIMA_URL = "http://79.72.62.113:8069/"  # dominio del proveedor
+        OPTIMA_URL = "http://79.72.55.217:8069/"  # dominio del proveedor
         OPTIMA_DB = "odoo0"
         OPTIMA_USER = "admin"
         OPTIMA_PASS = "admin"
