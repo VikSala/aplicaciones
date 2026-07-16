2a22262a1f0f764bdb7e38cf0b39cd172bd3a518
VikSala <vsz11697@gmail.com>
2026-07-16 17:52:24 +0200

    .

--- website_custom_filters/__manifest__.py
+++ website_custom_filters/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Filtro Slider Profesional Odoo 18',
-    'version': '5.6',
+    'version': '5.7',
     'depends': ['product', 'website_sale'],
     'license': 'LGPL-3',
     'data': [
